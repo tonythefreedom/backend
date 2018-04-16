936ce3ef40222f2e73f1a1d8b416ef0e98b72319
LeeKyuBum <lkb0938@hanmail.net>
2018-04-16 14:39:00 +0900

    api1&2
    코드정리,
    입력,출력 시간 출력

--- api/views.py
+++ api/views.py
@@ -9,20 +9,21 @@ import pandas
 from nltk.corpus import stopwords
 import csv
 from operator import itemgetter
+import datetime
+import sys
+sys.stdout.flush()
 
 #######################################################################################################
-#   API1 Function
+# Setting
 #######################################################################################################
 test_str = 'burgerking'
 test_str = test_str.lower()
 
 #local path
-#MODEL_PATH = "/Users/mac/model/gensim_model_skip_gram"
-#TABLE_PATH = "/Users/mac/model/word_count_table.json"
-
-#MODEL_PATH = "/Users/kyubum/PycharmProjects/simility/kb_test/model/gensim_model_skip_gram"
-#TABLE_PATH = "/Users/kyubum/PycharmProjects/simility/kb_test/word_count_table.json"
-#WM_PATH = "/Users/kyubum/PycharmProjects/simility/kb_test/weight_df.csv"
+#MODEL_PATH = "/Users/kyubum/PycharmProjects/API1n2_Final/create_model/embedding_model_SkipGram"
+#TABLE_PATH = "/Users/kyubum/PycharmProjects/API1n2_Final/create_model/word_count_table.json"
+#WM_PATH = "/Users/kyubum/PycharmProjects/API1n2_Final/create_model/weight_matrix.csv"
+#N_GRAM_CASE_PATH = "/Users/kyubum/PycharmProjects/API1n2_Final/create_model/n_gram_case_dic.json"
 
 #Server path
 MODEL_PATH = "/usr/local/etc/django/model/gensim_model_skip_gram"
@@ -31,17 +32,17 @@ WM_PATH = "/usr/local/etc/django/model/weight_df.csv"
 
 
 
-model = Word2Vec.load(MODEL_PATH)     #yelp review skip_gram으로 돌린 모델#
+model = Word2Vec.load(MODEL_PATH)
 with open(TABLE_PATH) as f:
     term_frequencys = f.readlines()
 
-###############################
+
+#######################################################################################################
+#   API1 Functions
+#######################################################################################################
 # return frequency function
-###############################
 term_frequency_list = [json.loads(term_frequency, encoding = 'utf-8') for term_frequency in term_frequencys]
 term_frequency_dict = term_frequency_list[0]
-
-#return frequency function (input lower 전처리 추가해야함.)
 def return_frequency(word):
     word = word.lower()
     frequency = 0
@@ -50,12 +51,8 @@ def return_frequency(word):
     except :
         frequency = frequency
     return frequency
-#print(return_frequency('nicsdfe'))
 
-
-#################################
 # input word -> similar word list return function(output : 2차원 리스트)
-#################################
 def return_similar_word_list(word):
     similar_output_list = []
     xx = model.most_similar(positive = [str(word)], topn = 40)
@@ -69,20 +66,15 @@ def return_similar_word_list(word):
                 xx.remove(j)
         similar_output_list.append(similar_tem_list)
     return similar_output_list
-#print(return_similar_word_list(test_str))
 
-
+# input word -> similar word list return function(세부항목 name 모두)
 def return_similar_word_name(kb_list):
     similar_output_list_name = []
     for i in kb_list:
         similar_output_list_name.append(i[0])
     return similar_output_list_name
 
-
-
-###################################################################
 # similar_word_list -> average frequency function (output: 1차원 리스트)
-###################################################################
 def return_avg_frequency(word_list):
     similar_word_frequency = []
     for i in word_list:
@@ -94,22 +86,46 @@ def return_avg_frequency(word_list):
         similar_word_frequency.append(int(avg))
     return(similar_word_frequency)
 
+# Final_API1_function
+def API1(input_keyword):
+    input_keyword = input_keyword.lower()
+    input_keyword = input_keyword.strip()
+    input_keyword = input_keyword.replace(' ','_')
+    out_list = []
+    try :
+        if return_similar_word_list(input_keyword) != []:
+            kb = return_similar_word_list(input_keyword)
+            dic_name = return_similar_word_name(kb)
+            dic_frequency = return_avg_frequency(kb)
+            for i in range(len(dic_name)):
+                out_dict = {}
+                out_dict['name'] = dic_name[i]
+                out_dict['frequency'] = dic_frequency[i]
+                out_list.append(out_dict)
+
+            out_list3 = sorted(out_list, key=itemgetter('frequency'), reverse = True)
+            result = json.dumps(out_list3)
+
+    except KeyError :
+        out_list = ['No Result']
+        result = json.dumps(out_list)
+
+    return(result)
+
+
 
 #######################################################################################################
 #   API2 Function
 #######################################################################################################
 stop_words = set(stopwords.words('english'))
-##########################################################################
-# Load Weight_matrix  (weight_matrix는 API2_create_WM.py 코드에서 만들 수 있음.)
-##########################################################################
+
+# Load Weight_matrix
 weight_final_df = pandas.read_csv(WM_PATH, encoding = 'utf-8')
 weight_final_df.index = ['food','service','ambience','value']
 weight_final_df = weight_final_df.drop('Unnamed: 0',1)
 col_list = list(weight_final_df.columns.values)
 
-##########################################################
-# return taste, price, value, ambience score function
-##########################################################
+# Return Score Function
 def API2_function(input_list):
     menu_name = input_list
     food_score_list = []
@@ -160,49 +176,38 @@ def API2_function(input_list):
     #out_json = json.dumps(out_list)
     return(out_list)
 
-######################################################################################
+
+def API2(menu_list):
+    test_list = menu_list.split(',')
+    out_list = API2_function(test_list)
+    out_before_sort = sorted(out_list, key=itemgetter('avg_score'))
+    if len(out_before_sort) > 3:
+        out_list_sorting = [out_before_sort[-1], out_before_sort[-2], out_before_sort[-3], out_before_sort[-4]]
+    elif len(out_before_sort) == 3:
+        out_list_sorting = [out_before_sort[-1], out_before_sort[-2], out_before_sort[-3]]
+    elif len(out_before_sort) == 2:
+        out_list_sorting = [out_before_sort[-1], out_before_sort[-2]]
+    else:
+        out_list_sorting = [out_before_sort[-1]]
+
+    result = json.dumps(out_list_sorting)
+    return(result)
+#######################################################################################################
 # 리퀘스트 id 로 파라미터 보내기
-######################################################################################
+#######################################################################################################
 result = "{}"
 def index(request):
     if request.GET["id"] == "nlp1" :
         test_str = request.GET["keyword"]
-        test_str = test_str.lower()
-        out_list = []
-        try :
-            if return_similar_word_list(test_str) != []:
-                kb = return_similar_word_list(test_str)
-                dic_name = return_similar_word_name(kb)
-                dic_frequency = return_avg_frequency(kb)
-                for i in range(len(dic_name)):
-                    out_dict = {}
-                    out_dict['name'] = dic_name[i]
-                    out_dict['frequency'] = dic_frequency[i]
-                    out_list.append(out_dict)
-
-                sort_on = 'frequency'
-                out_list2 = [(dict_[sort_on], dict_) for dict_ in out_list]
-                out_list2.sort(reverse = True)
-                out_list3 = [dict_ for (key, dict_) in out_list2]
-                out_list4 = out_list3[0:6]    
-                result = json.dumps(out_list4)
-        except KeyError :
-            out_list = ['No Result']
-            result = json.dumps(out_list)
+        print("API1_Starts : " , datetime.datetime.now().time(), flush = True)
+        result = API1(test_str)
+        print("API1_Ends   : " , datetime.datetime.now().time() , " : " , result, flush = True)
 
     elif request.GET["id"] == "nlp2" :
         test_str = request.GET["menu_list"]
-        test_list = test_str.split(',')
-        out_list = API2_function(test_list)
-        out_before_sort = sorted(out_list, key=itemgetter('avg_score'))
-        if len(out_before_sort) > 3:
-            out_list_sorting = [out_before_sort[-1], out_before_sort[-2], out_before_sort[-3], out_before_sort[-4]]
-        elif len(out_before_sort) == 3:
-            out_list_sorting = [out_before_sort[-1], out_before_sort[-2], out_before_sort[-3]]
-        elif len(out_before_sort) == 2:
-            out_list_sorting = [out_before_sort[-1], out_before_sort[-2]]
-        else:
-            out_list_sorting = [out_before_sort[-1]]
-        
-        result = json.dumps(out_list_sorting)        
+        #n_gram_case_dic에 포함된 문자는 ' ' -> '_' 전처리 필요!
+        print("API2_Starts : " , datetime.datetime.now().time(), flush = True)
+        result = API2(test_str)
+        print("API2_Ends   : " , datetime.datetime.now().time() , " : " , result, flush = True)
+    
     return HttpResponse(result)
